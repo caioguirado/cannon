38260107cee6aa9f5609fe2fb67000cbf506a871
Caio Guirado <caioguirado@yahoo.com.br>
2021-09-06 22:49:11 +0200

    Add side/top notations

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import {pieces} from './pieces';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {DndProvider} from 'react-dnd';
+import {Board} from './components/Board';
+import {Container} from './components/Container';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './App.scss';
+import { GameArea } from './components/GameArea';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return  <GameArea>
+                <Container>
+                    <DndProvider backend={HTML5Backend}> 
+                        <Board nCells={20} pieces={pieces}/>
+                    </DndProvider>
+                </Container>
+            </GameArea>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
